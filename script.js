809927f3fd01315ed265ac3c9be117afb70a4251
Whaleless <97296345+Whaleless@users.noreply.github.com>
2022-10-17 20:54:49 +0200

    lots_of_things

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let dynamicDate = document.querySelector('.dynamic-expiration-date');
 let dynamicCvv = document.querySelector('.dynamic-cvv');
 
 //Form Data
+let form = document.querySelector('.card-details-form');
 let formName = document.querySelector('.name');
 let formNumber = document.querySelector('.number');
 let formMonth = document.querySelector('.month');
@@ -13,15 +14,148 @@ let formYear = document.querySelector('.year');
 let formCvv = document.querySelector('.cvv');
 let confirmButton = document.querySelector('.submit-button');
 
-//Helper
-String.prototype.replaceAt = function(index, replacement) {
-    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-}
+//success
+let successBlock = document.querySelector('.success-status');
+let continueButton = document.querySelector('.continue-button');
 
 //--------------------------------Form Data Validation--------------------------------//
+
+//Checks
+
+function isNotEmptyCheck(input) {
+    if (input.value == '') {
+        input.parentNode.classList.add('error_empty');
+        return true;
+    } else {
+        input.parentNode.classList.remove('error_empty');
+        return false;
+    };
+}
+
+function containsDigits(input) {
+    if (/\d+/.test(input.value)) {
+        input.parentNode.classList.add('error_digits');
+        return true;
+    } else {
+        input.parentNode.classList.remove('error_digits');
+        return false;
+    };
+}
+
+function containsLetters(input) {
+    if (/[a-zA-Z]+/.test(input.value)) {
+        input.parentNode.classList.add('error_letters');
+        return true;
+    } else {
+        input.parentNode.classList.remove('error_letters');
+        return false;
+    };
+}
+
+function isTooLittle(input, howMuch) {
+    if (input.value < howMuch && input.value != '') {
+        input.parentNode.classList.add('error_too_little');
+        return true;
+    } else {
+        input.parentNode.classList.remove('error_too_little');
+        return false;
+    };
+}
+
+function containsNotEnoughSymbols(input, howMuch) {
+    if (input.value.length < howMuch && input.value != '') {
+        input.parentNode.classList.add('error_not_enough');
+        return true;
+    } else {
+        input.parentNode.classList.remove('error_not_enough');
+        return false;
+    };
+}
+
+//inputsCheckers
+//Name
+function nameInputCheckers(input) {
+    let empty = isNotEmptyCheck(input);
+    let digits = containsDigits(input);
+
+    let errors = empty || digits;
+    if (errors) {
+        input.classList.add('error');
+        return true;
+    } else {
+        input.classList.remove('error');
+        return false;
+    }
+}
+
+//Number
+function numberInputCheckers(input) {
+    let empty = isNotEmptyCheck(input);
+    let notEnough = containsNotEnoughSymbols(input, 19);
+
+    let errors = empty || notEnough;
+    if (errors) {
+        input.classList.add('error');
+        return true;
+    } else {
+        input.classList.remove('error');
+        return false;
+    }
+}
+
+//Month
+function monthInputCheckers(input) {
+    let empty = isNotEmptyCheck(input);
+
+    if (empty) {
+        input.classList.add('error');
+        return true;
+    } else {
+        input.classList.remove('error');
+        return false;
+    }
+}
+
+//Year
+function yearInputCheckers(input) {
+    let year = (new Date().getFullYear()).toString().substring(2,);
+
+    let empty = isNotEmptyCheck(input);
+    let tooLittle = isTooLittle(input, year);
+
+    let errors = empty || tooLittle;
+    if (errors) {
+        input.classList.add('error');
+        return true;
+    } else {
+        input.classList.remove('error');
+        return false;
+    }
+}
+
+//CVV
+function cvvInputCheckers(input) {
+    let empty = isNotEmptyCheck(input);
+    let letters = containsLetters(input);
+    let notEnough = containsNotEnoughSymbols(input, 3);
+
+    let errors = empty || letters || notEnough;
+    if (errors) {
+        input.classList.add('error');
+        return true;
+    } else {
+        input.classList.remove('error');
+        return false;
+    }
+}
+
 //Name
 setInputFilter(formName, function(value) {
     return /^(([a-zA-Z\d]{0,15})( )?([a-zA-Z\d]){0,15})$/.test(value);
+});
+
+formName.addEventListener('change', () => {
+    nameInputCheckers(formName);
 });
 
 //Number
@@ -40,17 +174,37 @@ formNumber.addEventListener('input', (e)=> {
     numberChanger(e);
 })
 
+formNumber.addEventListener('change', () => {
+    numberInputCheckers(formNumber);
+})
+
 //Month
 setInputFilter(formMonth, function(value) {
     return /^(0?[1-9]?)?(1?[0-2]?)?$/.test(value);
 });
 
 formMonth.addEventListener('input', (e) => {
-    if (formMonth.value[0] != '1' && formMonth.value[0] != '0' && e.data != null) {
-        formMonth.value = '0' + e.data;
+    if (/\d/.test(e.data) || e.data == null) {
+        if (formMonth.value[0] != '1' && formMonth.value[0] != '0' && e.data != null) {
+            formMonth.value = '0' + e.data;
+        }
+
+        monthChanger(e);
+    }
+})
+
+formMonth.addEventListener('change', (e) => {
+    if(formMonth.value == '1') {
+        formMonth.value = '01';
+    } else if (formMonth.value == 0) {
+        formMonth.value = '';
     }
 
     monthChanger(e);
+})
+
+formMonth.addEventListener('change', () => {
+    monthInputCheckers(formMonth);
 })
 
 //Year
@@ -58,12 +212,20 @@ setInputFilter(formYear, function(value) {
     return /^\d{0,2}$/.test(value);
 });
 
-//CVV
-setInputFilter(formCvv, function(value) {
-    return /^\d{0,3}$/.test(value);
+formYear.addEventListener('change', () => {
+    yearInputCheckers(formYear);
 });
 
-//Helper
+//CVV
+setInputFilter(formCvv, function(value) {
+    return /^[\da-zA-Z]{0,3}$/.test(value);
+});
+
+formCvv.addEventListener('change', () => {
+    cvvInputCheckers(formCvv);
+});
+
+//Helpers
 function setInputFilter(textbox, inputFilter) {
     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
         textbox.addEventListener(event, function() {
@@ -82,6 +244,18 @@ function setInputFilter(textbox, inputFilter) {
 }
 
 //------------------------------------Data Changer------------------------------------//
+
+//Helper
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
+function defaultValues() {
+    dynamicName.innerHTML = 'Jane Appleseed';
+    dynamicNumber.innerHTML = '0000 0000 0000 0000';
+    dynamicDate.innerHTML = '00/00';
+    dynamicCvv.innerHTML = '000';
+}
 
 //Name
 formName.addEventListener('input', (e) => {
@@ -138,7 +312,7 @@ formYear.addEventListener('input', (e) => {
 
 //CVV
 formCvv.addEventListener('input', (e) => {
-    if(/\d/.test(e.data)) {
+    if(/[\da-zA-Z]/.test(e.data)) {
         for (let i = 0; i < formCvv.value.length; i++) {
             dynamicCvv.innerHTML = dynamicCvv.innerHTML.replaceAt(i, formCvv.value[i]);
         }
@@ -152,7 +326,46 @@ formCvv.addEventListener('input', (e) => {
 
 //------------------------------------Confirmation------------------------------------//
 
-confirmButton.addEventListener('click', () => {
+function successChanger() {
+    form.classList.toggle('hidden');
+    successBlock.classList.toggle('hidden');
+}
 
+function FormValidation() {
+    let errors = nameInputCheckers(formName);
+    errors = errors || numberInputCheckers(formNumber);
+    errors = errors || monthInputCheckers(formMonth);
+    errors = errors || yearInputCheckers(formYear);
+    errors = errors || cvvInputCheckers(formCvv);
+
+    return errors;
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+    if(!FormValidation()) {
+        const data = serializeForm(event.target)
+        const {status} = await sendData(data);
+        form.reset();
+        successChanger();
     }
-)
+}
+
+function serializeForm(formNode) {
+    return new FormData(formNode)
+}
+
+async function sendData(data) {
+    return await fetch('#', {
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: data,
+    })
+}
+
+form.addEventListener('submit', handleFormSubmit);
+
+continueButton.addEventListener('click', () => {
+    defaultValues();
+    successChanger();
+})
